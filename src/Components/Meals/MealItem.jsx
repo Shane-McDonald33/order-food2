49c3310af4271a5768eaed1268dfecb788e05462
Shane-McDonald33 <mcdonaldshane33@gmail.com>
2022-02-27 20:48:43 -0600

    more styling

--- src/Components/Meals/MealItem.jsx
+++ src/Components/Meals/MealItem.jsx
@@ -1,5 +1,5 @@
 import classes from './MealItem.module.css';
-
+import MealForm from './MealForm'
 
 const MealItem = (props) => {
     return (
@@ -8,6 +8,9 @@ const MealItem = (props) => {
                 <h3>{props.name}</h3>
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{props.price}</div>
+            </div>
+            <div>
+                <MealForm/>
             </div>
         </li>
     )
